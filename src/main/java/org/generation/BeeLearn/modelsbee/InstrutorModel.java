024d6d4cc8773d7b6676de6df46f94b70cf8a77d
lucas apalosqui <lucas.apalosqui@gmail.com>
2021-12-09 15:18:43 -0300

    @lucasApalosqui [Task: Update]

--- src/main/java/org/generation/BeeLearn/modelsbee/InstrutorModel.java
+++ src/main/java/org/generation/BeeLearn/modelsbee/InstrutorModel.java
@@ -13,12 +13,8 @@ import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-<<<<<<< HEAD
-import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-=======
->>>>>>> 4a44cc6244556b080d95057f81062faafff682d8
 
 @Entity
 @Table(name = "tbl_instrutor")
@@ -30,19 +26,6 @@ public class InstrutorModel {
 	private @NotBlank String bioInstrutor;
 	private @NotNull Long nivelInstrutor;
 	private @NotNull Long xpInstrutor;
-
-<<<<<<< HEAD
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
-
-	@NotNull
-	@Size(min = 1, max = 500)
-	private String url_instrutor;
-	private String titulo_instrutor;
-	private String bio_instrutor;
-	private int nivel_instrutor;
-	private int xp_instrutor;
 
 	@OneToOne(mappedBy = "instrutor")
 	private UserModel userIns;
@@ -59,46 +42,6 @@ public class InstrutorModel {
 		this.id = id;
 	}
 
-	public String getUrl_instrutor() {
-		return url_instrutor;
-	}
-
-	public void setUrl_instrutor(String url_instrutor) {
-		this.url_instrutor = url_instrutor;
-	}
-
-	public String getTitulo_instrutor() {
-		return titulo_instrutor;
-	}
-
-	public void setTitulo_instrutor(String titulo_instrutor) {
-		this.titulo_instrutor = titulo_instrutor;
-	}
-
-	public String getBio_instrutor() {
-		return bio_instrutor;
-	}
-
-	public void setBio_instrutor(String bio_instrutor) {
-		this.bio_instrutor = bio_instrutor;
-	}
-
-	public int getNivel_instrutor() {
-		return nivel_instrutor;
-	}
-
-	public void setNivel_instrutor(int nivel_instrutor) {
-		this.nivel_instrutor = nivel_instrutor;
-	}
-
-	public int getXp_instrutor() {
-		return xp_instrutor;
-	}
-
-	public void setXp_instrutor(int xp_instrutor) {
-		this.xp_instrutor = xp_instrutor;
-	}
-
 	public UserModel getUserIns() {
 		return userIns;
 	}
@@ -113,9 +56,6 @@ public class InstrutorModel {
 
 	public void setGrupo(List<GrupoModels> grupo) {
 		this.grupo = grupo;
-=======
-	public Long getId() {
-		return id;
 	}
 
 	public String getUrlInstrutor() {
@@ -142,8 +82,6 @@ public class InstrutorModel {
 		this.bioInstrutor = bioInstrutor;
 	}
 
-	
-
 	public Long getNivelInstrutor() {
 		return nivelInstrutor;
 	}
@@ -162,7 +100,7 @@ public class InstrutorModel {
 
 	public void setId(Long id) {
 		this.id = id;
->>>>>>> 4a44cc6244556b080d95057f81062faafff682d8
+
 	}
 
 }
