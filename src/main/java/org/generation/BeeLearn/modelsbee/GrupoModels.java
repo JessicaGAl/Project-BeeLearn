e2e88bc7e4c921e9884e788b476a8c481a04be76
Bianca Barros <biancabarrosufrj@gmail.com>
2021-12-10 14:56:08 -0300

    @biancabs2 [Task: Relationship between tables UserModel And GrupoModel]

--- src/main/java/org/generation/BeeLearn/modelsbee/GrupoModels.java
+++ src/main/java/org/generation/BeeLearn/modelsbee/GrupoModels.java
@@ -7,8 +7,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 
 @Entity
@@ -20,7 +24,19 @@ public class GrupoModels {
 	private @NotBlank String descricao;
 	private String urlImagem;
 	
-	
+	@ManyToOne
+	@JsonIgnoreProperties("GrupoModels")
+	@JoinColumn(name = "idUsuario")
+	private UserModel user;
+
+
+	public UserModel getUser() {
+		return user;
+	}
+
+	public void setUser(UserModel user) {
+		this.user = user;
+	}
 
 	public Long getIdGrupo() {
 		return idGrupo;
